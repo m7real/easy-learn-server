7e17d2d838157c0c677e66c0fc930b6d5157c5f1
m7real <masayeedreal@gmail.com>
2022-10-25 23:04:07 +0600

    course image and details added

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const port = process.env.PORT || 5000;
 // cors used to handle cross origin resource sharing
 app.use(cors());
 
+const courses = require("./data/courses.json");
+
 // api on root route for testing purpose
 app.get("/", (req, res) => {
   res.send("API RUNNING");
+});
+
+// API to get all courses
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
