cac9edf66267f805ce3f1027407e5ea28eb0bddd
m7real <masayeedreal@gmail.com>
2022-10-25 20:49:25 +0600

    fake data added

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+// cors used to handle cross origin resource sharing
 app.use(cors());
 
+// api on root route for testing purpose
 app.get("/", (req, res) => {
   res.send("API RUNNING");
 });
